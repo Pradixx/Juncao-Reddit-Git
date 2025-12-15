f684096af8f468dce9a5b7b9ee070918591a9102
Pradixx <dieguittosilva09@gmail.com>
2025-12-14 23:31:43 -0300

    UserService

--- src/main/java/com/digitodael/redgit/infrastructure/security/UserSecurity.java
+++ src/main/java/com/digitodael/redgit/infrastructure/security/UserSecurity.java
@@ -1,4 +1,26 @@
 package com.digitodael.redgit.infrastructure.security;
 
+import com.digitodael.redgit.infrastructure.entity.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component("userSecurity")
 public class UserSecurity {
+
+    public boolean isOwner(UUID userId) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null || !auth.isAuthenticated()) {
+            return false;
+        }
+
+        Object principal = auth.getPrincipal();
+        if (principal instanceof User user) {
+            return user.getId().equals(userId);
+        }
+
+        return false;
+    }
 }
