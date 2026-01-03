e73af622a3fff5fd865c047062bc3e45bf0a8b3c
Pradixx <dieguittosilva09@gmail.com>
2026-01-03 08:56:55 -0300

    Repositories

---
+++ src/main/java/com/redgit/profile/infrastructure/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.redgit.profile.infrastructure.repository;
+
+import com.redgit.profile.infrastructure.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
