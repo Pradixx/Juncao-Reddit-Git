6bcd01d62eed74199dd90c4e713c822822b77774
Pradixx <dieguittosilva09@gmail.com>
2026-01-03 08:56:18 -0300

    Entities

---
+++ src/main/java/com/redgit/profile/infrastructure/entities/User.java
@@ -0,0 +1,24 @@
+package com.redgit.profile.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
