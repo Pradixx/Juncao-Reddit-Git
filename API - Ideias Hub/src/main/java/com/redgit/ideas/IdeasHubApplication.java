258569ab70205aa206f497e7afee07867b73cecd
Pradixx <dieguittosilva09@gmail.com>
2026-01-04 10:11:47 -0300

    Removendo Bugs após pull da merge

--- API - Ideias Hub/src/main/java/com/redgit/ideas/IdeasHubApplication.java
+++ API - Ideias Hub/src/main/java/com/redgit/ideas/IdeasHubApplication.java
@@ -3,10 +3,11 @@ package com.redgit.ideas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@PropertySource("classpath:mongo.properties")
 public class IdeasHubApplication {
 
 	public static void main(String[] args) {
