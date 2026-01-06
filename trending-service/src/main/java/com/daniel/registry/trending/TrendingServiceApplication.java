3f2769f12ecd0689c2580fcb89ac49fb1964c34e
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2026-01-06 16:35:11 -0300

    first

---
+++ trending-service/src/main/java/com/daniel/registry/trending/TrendingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.registry.trending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendingServiceApplication.class, args);
+	}
+
+}
