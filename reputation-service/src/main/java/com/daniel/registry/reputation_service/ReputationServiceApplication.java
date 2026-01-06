3f58b03bf20f8a1d9532a3e30e76c8e235dec29d
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2026-01-06 18:59:16 -0300

    first

---
+++ reputation-service/src/main/java/com/daniel/registry/reputation_service/ReputationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.registry.reputation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReputationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReputationServiceApplication.class, args);
+	}
+
+}
---
+++ reputation-service/src/test/java/com/daniel/registry/reputation_service/ReputationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.registry.reputation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReputationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
